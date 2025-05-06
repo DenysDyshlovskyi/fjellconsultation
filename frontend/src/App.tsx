6441969b672d216fb4f40a73832083be39622559
MagiscCow <ddyshlovskyi@gmail.com>
2025-05-06 12:49:37 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route, Navigate} from "react-router";
+import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+
+export default function App() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Navigate to={'/register'} />}/>
+                <Route path={'/register'} element={<RegisterPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path={'/home'} element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
