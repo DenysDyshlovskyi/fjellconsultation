424ea86dcdbeb2a46b15cc7bce0c6e57cc7c3a1a
unknown <jakob.johannes.hansgaard@outlook.com>
2025-05-07 08:51:40 +0200

    second commit

---
+++ api/create_db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE fjell_consultation;
+
+\c fjell_consultation;
+
+CREATE TABLE tickets (
+    id UUID PRIMARY KEY,
+    title varchar(255),
+    description TEXT,
+    category int NOT NULL,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name varchar(127) NOT NULL
+);
+
+CREATE TABLE ticket_messages (
+    id UUID PRIMARY KEY,
+    ticket_id UUID NOT NULL,
+    message TEXT,
+);
+
+CREATE TABLE message_embeds (
+    id UUID PRIMARY KEY,
+    ticket_message_id UUID NOT NULL,
+    url TEXT NOT NULL
+);
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,38 @@
 import Express from "express";
 import "dotenv/config"
+import { Pool } from 'pg'
 
 // Defines variables for later use
 const app = Express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.json({"message": "heisann"})
+// Set up database connection
+const pool = new Pool({
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT),
+    database: process.env.PGDATABASE,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+})
+
+// Endpoint for submitting a ticket
+app.post('/tickets', (req, res) => {
+    // Retrieve post data
+    var title = req.body.title
+    var description = req.body.description
+    var category = req.body.category
+})
+
+app.get('/tickets/:id', (req, res) => {
+    const ticket_id = req.params.id
+})
+
+// If route was not found
+app.all(/(.*)/, (req, res) => {
+    res.json({"status": "404"})
 })
 
 // Run server on port const
