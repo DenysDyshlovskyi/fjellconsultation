efbeeb30899dd004e3e51cb42b653979e1a8338a
MagiscCow <ddyshlovskyi@gmail.com>
2025-05-08 13:38:21 +0200

    basic viewticketpage layout + form submission post request

--- frontend/src/pages/NewTicketPage.tsx
+++ frontend/src/pages/NewTicketPage.tsx
@@ -1,4 +1,5 @@
 import {ChangeEvent, useState} from 'react';
+import axios from 'axios';
 
 export default function NewTicketPage() {
     const [ticketForm, setTicketForm] = useState({
@@ -16,24 +17,39 @@ export default function NewTicketPage() {
 
     const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(ticketForm)
+        e.target.reset();
+        axios.post('/api/tickets', {
+                title: ticketForm.title,
+                description: ticketForm.description,
+                category: ticketForm.category,
+            }
+        ).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+            alert('Something went wrong');
+        });
     }
 
     return (
         <div className={'w-full h-screen flex justify-center items-center bg-brand/20 p-4'}>
             <div
-                className={'max-w-full w-6xl border-1 border-brand-alt/25 rounded-lg flex flex-col bg-main justify-center items-center p-5 gap-2'}>
+                className={'max-w-full w-2xl border-1 border-brand-alt/25 rounded-lg flex flex-col bg-main justify-center items-center p-5 gap-2'}>
                 <p className={'text-2xl'}>Submit new Ticket</p>
-                <form onSubmit={handleSubmit}>
-                    <input name={'title'} className={'border-1 rounded-md border-brand px-3 py-2 w-full'}
-                           placeholder={'Title'} onChange={handleChange}></input>
-                    <select name={'category'} className={'border-1 rounded-md border-brand px-3 py-2 w-full mt-3'} onChange={handleChange}>
+                <form onSubmit={handleSubmit} className={'flex flex-col gap-4 w-full'}>
+                    <input required name={'title'} className={'border-1 rounded-md border-brand px-3 py-2 w-full'}
+                           placeholder={'Title'} onChange={handleChange}/>
+                    <select required name={'category'} className={'border-1 rounded-md border-brand px-3 py-2 w-full'}
+                            onChange={handleChange}>
                         <option selected disabled>Category</option>
                         <option>Option 1</option>
                         <option>Option 2</option>
                     </select>
-                    <textarea name={'description'} placeholder={'Message'} className={'border-brand rounded-md border-1 w-full h-full my-3 p-2'} onChange={handleChange}></textarea>
-                    <button className={'bg-brand text-white rounded-md px-3 py-2'} type={'submit'}>Submit Ticket</button>
+                    <textarea required name={'description'} placeholder={'Message'}
+                              className={'border-brand rounded-md border-1 w-full h-full px-4 py-2'}
+                              onChange={handleChange}/>
+                    <button className={'bg-brand text-white rounded-md px-3 py-2'} type={'submit'}>Submit Ticket
+                    </button>
                 </form>
                 <button className={'bg-brand text-white rounded-md px-3 py-2'} type={'submit'}>Save Draft</button>
 
--- frontend/src/pages/ViewTicketsPage.tsx
+++ frontend/src/pages/ViewTicketsPage.tsx
@@ -1,9 +1,58 @@
 export default function ViewTicketsPage() {
     return (
-        <div className={'w-full h-screen flex justify-center items-center flex bg-brand/20 p-4'}>
+        <div className={'w-full h-screen flex justify-center items-center bg-brand/20 p-4'}>
             <div
                 className={'max-w-full w-6xl border-1 border-brand-alt/25 rounded-lg flex flex-col bg-main justify-center items-center p-5 gap-4'}>
                 <p className={'text-2xl'}>Your tickets</p>
+                <table className={'w-full table-auto overflow-y-scroll'}>
+                    <thead className={'w-full h-16 border-1'}>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-xl font-bold m-auto'}>Ticket ID</td>
+                            <td className={'w-full text-xl font-bold m-auto'}>Title</td>
+                            <td className={'w-full text-xl font-bold m-auto'}>Category</td>
+                            <td className={'w-full text-xl font-bold m-auto'}>Description</td>
+                        </tr>
+                    </thead>
+                    <tbody className={'w-full'}>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-2xl font-bold m-auto'}># 123456</td>
+                            <td className={'w-full text-md m-auto'}>My Big Problem</td>
+                            <td className={'w-full text-md m-auto'}>Option 1</td>
+                            <td className={'w-full text-md m-auto'}>I need help please</td>
+                        </tr>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-2xl font-bold m-auto'}># 123456</td>
+                            <td className={'w-full text-md m-auto'}>My Big Problem</td>
+                            <td className={'w-full text-md m-auto'}>Option 1</td>
+                            <td className={'w-full text-md m-auto'}>I need help please</td>
+                        </tr>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-2xl font-bold m-auto'}># 123456</td>
+                            <td className={'w-full text-md m-auto'}>My Big Problem</td>
+                            <td className={'w-full text-md m-auto'}>Option 1</td>
+                            <td className={'w-full text-md m-auto'}>I need help please</td>
+                        </tr>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-2xl font-bold m-auto'}># 123456</td>
+                            <td className={'w-full text-md m-auto'}>My Big Problem</td>
+                            <td className={'w-full text-md m-auto'}>Option 1</td>
+                            <td className={'w-full text-md m-auto'}>I need help please</td>
+                        </tr>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-2xl font-bold m-auto'}># 123456</td>
+                            <td className={'w-full text-md m-auto'}>My Big Problem</td>
+                            <td className={'w-full text-md m-auto'}>Option 1</td>
+                            <td className={'w-full text-md m-auto'}>I need help please</td>
+                        </tr>
+                        <tr className={'w-full h-16 bg-brand/20 flex flex-row items-center justify-center px-5'}>
+                            <td className={'w-full text-2xl font-bold m-auto'}># 123456</td>
+                            <td className={'w-full text-md m-auto'}>My Big Problem</td>
+                            <td className={'w-full text-md m-auto'}>Option 1</td>
+                            <td className={'w-full text-md m-auto'}>I need help please</td>
+                        </tr>
+
+                    </tbody>
+                </table>
                 <a href={'/home'} className={'text-xl text-link underline'}>Back to Home</a>
             </div>
         </div>
